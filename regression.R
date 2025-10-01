44fbf5f48215fe7fb86294c62036f54d3c9fa334
Sigurd Dam Sonniks <Sigurddam@hotmail.com>
2025-10-01 20:49:37 +0200

    Create column days_till_censoring, make marginal dist. plots and plots of time-to-event columns separated by their indicator

--- regression.R
+++ regression.R
@@ -5,45 +5,130 @@ library(splines)
 library(readr)
 library(skimr)
 library(dplyr)
+library(rlang)
 
-framingham <- as_tibble(read.csv("training_data.csv"))
+set.seed(8088)
+framingham_ <- as_tibble(read.csv("training_data.csv"))
 
 
 # Framingham is ordered so that lower RANDIDs come first, and when there are
-# multiple rows for the same RANDID, the lower ages come first.
+# multiple rows for the same RANDID, the earlier checkups come first.
 # This is evidenced by the below check.
-all(arrange(framingham[,c("RANDID","AGE")]) == framingham[,c("RANDID","AGE")])
-
-# We use this knowledge to add the following columns:
-# - age_diff shows how much each person has aged since their first checkup.
-# - checkup shows which checkup number the row is.
-# - years_till_censoring shows how many years of observation are left until the censoring date.
-# We also drop X since it just counts rows.
-fram_checkup <- mutate(
-    framingham,
-    age_diff=AGE-AGE[1],
-    # Choices for cutoffs are argued below.
-    checkup=ifelse(age_diff == 0, 1, ifelse(age_diff < 10, 2, 3)),
-    years_till_censoring=24-age_diff,
-    X=NULL,
-    .by=RANDID,
-    .after=RANDID
-)
-
-# The following expression shows how many examinations were done roughly how
-# many years after each person's first checkup. Note that this is not the
-# same as calendar years since the first observation.
-# The checkups are approimately 6 years apart, so it seems reasonable that
-# the one observation after 8 years would belong to the second checkup round,
-# while the three checkups after ten years would belong to the third. This way, each
-# checkup round runs over a consecutive number of years (as measured by age_diff).
-table(fram_checkup$age_diff)
+all(arrange(framingham_[,c("RANDID","TIME")]) == framingham_[,c("RANDID","TIME")])
 
 # Thankfully noone attended more than three examinations.
-max(fram_checkup$checkup)
+max(table(framingham_[,"RANDID"]))
 
-# The following expression tells use that over half of the observations
+
+# We use this knowledge to add the column days_till_censoring which shows
+# how many years of observation are left until the censoring date. This is
+# based on some loose estimate of the censoring date being after 24 years.
+# We also drop X since it just counts rows and reencode a bunch of columns
+# as factors. Not CVD though since it's our response and so it has to be
+# numeric if we want to fit regular lms on it.
+framingham <- mutate(
+    framingham_,
+    SEX      = factor(SEX,      ordered=TRUE),
+    CURSMOKE = factor(CURSMOKE, ordered=TRUE),
+    DIABETES = factor(DIABETES, ordered=TRUE),
+    BPMEDS   = factor(BPMEDS,   ordered=TRUE),
+    educ     = factor(educ,     ordered=TRUE),
+    PREVCHD  = factor(PREVCHD,  ordered=TRUE),
+    PREVAP   = factor(PREVAP,   ordered=TRUE),
+    PREVMI   = factor(PREVMI,   ordered=TRUE),
+    PREVSTRK = factor(PREVSTRK, ordered=TRUE),
+    PREVHYP  = factor(PREVHYP,  ordered=TRUE),
+    PERIOD   = factor(PERIOD,   ordered=TRUE),
+    DEATH    = factor(DEATH,    ordered=TRUE),
+    ANGINA   = factor(ANGINA,   ordered=TRUE),
+    HOSPMI   = factor(HOSPMI,   ordered=TRUE),
+    MI_FCHD  = factor(MI_FCHD,  ordered=TRUE),
+    ANYCHD   = factor(ANYCHD,   ordered=TRUE),
+    STROKE   = factor(STROKE,   ordered=TRUE),
+    HYPERTEN = factor(HYPERTEN, ordered=TRUE),
+    # We can see that the censoring date was after 8766 days because that's
+    # the maximum value in all the TIME* columns, and I think people who didn't
+    # have a specific event happen were registered as having it occur after the
+    # whole examination period.
+    days_till_censoring=8766-TIME,
+    X=NULL,
+)
+
+# The following expression tells us that over half of the observations
 # are in the second and third checkups, so it would be a shame to have to
 # throw all that away.
-table(fram_checkup$checkup)
+table(framingham$PERIOD)
+
+
+# A list of the correspondences between the times of events
+# and the indicators of those events.
+timecols <- list(
+    TIMEAP   = "ANGINA",
+    TIMEMI   = "HOSPMI",
+    TIMEMIFC = "MI_FCHD",
+    TIMECHD  = "ANYCHD",
+    TIMESTRK = "STROKE",
+    TIMECVD  = "CVD",
+    TIMEDTH  = "DEATH",
+    TIMEHYP  = "HYPERTEN"
+)
+
+fill = gray(0.7)
+
+# For a column giving the time of occurrence of some event,
+# create two plot showing the density of those times.
+# One where the indicator is zero and one where it is one.
+timeplot <- function(col) {
+    indicator <- timecols[[col]]
+    ggplot(framingham) +
+        facet_grid(expr(. ~ !!sym(indicator)), label=label_both) +
+        aes_string(col) +
+        geom_density(fill=fill)
+}
+
+# Create such plots for all the time-to-event columns.
+# We see that the distribution are quite different,
+# in particular the extremely heavy tail. If the event didn't occur,
+# the last moment of contact is registered in the time-to-event column
+# instead. So we have to filter the rows out where the event
+# didn't occur when making plots of these columns if they are to
+# make any sense.
+do.call(gridExtra::grid.arrange, c(lapply(names(timecols), timeplot), ncol=4))
+
+# Filter the data such that it only contains the rows
+# where some event happened. The parameter col is supposed to
+# be the column containing the times to the event, not the indicator
+# of the event. E.g. TIMEAP, not ANGINA.
+timefilter <- function(col, data=framingham, indicator_value=1) {
+    indicator <- timecols[[col]]
+    filter(data, data[[indicator]] == indicator_value)
+}
+
+# Plot the marginal distribution of a column
+marginal_plot <- function(col) {
+    if (is.factor(framingham[[col]]) | col == "CVD") {
+        plot <- ggplot(data=framingham) +
+            aes_string(col) +
+            xlab(col) +
+            ylab("") +
+            geom_bar(fill=fill)
+    } else {
+        if (is.null(timecols[[col]])) {
+            data <- framingham
+        } else {
+            data <- timefilter(col)
+        }
+        plot <- ggplot(data=data) +
+            aes_string(col) +
+            xlab(col) +
+            ylab("") +
+            geom_density(fill=fill)
+    }
+    plot
+}
+
+tmp <- lapply(names(framingham), marginal_plot)
+do.call(gridExtra::grid.arrange, c(tmp, ncol=5))
+
+skim(framingham)
 
